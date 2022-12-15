9796b022ada6adefe95ef12935c456f4de49bc44
Avass16 <andreyvass08@gmail.com>
2022-12-15 18:57:00 +0600

    dfssdf

--- projects/blackjack21.py
+++ projects/blackjack21.py
@@ -1,101 +1,115 @@
-#  колода из 36 карт, перемешанная в случайном порядке
+# колода из 36 карт, перемешанная в случайном порядке
 
-# ри взятии карты прибавляет ее номинал к своим очкам
+# при взятии карты игрок прибавляет ее номинал к своим очкам
 
 # взятая карта удаляется из колоды
 
 # побеждает тот игрок, который набрал 21 очко
 
-# если никто не набрал 21, побеждает тот, кто набрал ближе к 21
+# если никто не набрал 21, побеждает тот, кто набрал ближе всего к 21 
 import random
+
 def setup_deck():
     deck = [2,3,4,6,7,8,9,10,11]*4
     random.shuffle(deck)
-    print(deck)
     return deck
- 
+    print(deck)
+
 def draw_a_card(deck):
-    if len(deck) !=0:
-       card = deck.pop(-1) 
+    if len(deck) != 0:  # if deck:
+        card = deck.pop(-1)
     else:
-        card = None
+        card = None    
     return card
+
 def update_score(current_score, score_amount):
-    current_score = current_score + score_amount
+    current_score = current_score + score_amount # current_score += score_amount
     return current_score
 
 def count_score(score):
     if score == 21:
         return True
-    elif score >21:
+    elif score > 21:
         return False
-    elif score <21:
-         return None
-def draw_decision(text):
-    if text == 'y':
-      return True
-    elif text == 'n':
-      return False
-    else:
+    elif score < 21:
         return None
 
-def setup_players(num_players =1):
+def draw_decision(text):
+    if text == 'y':
+        return True
+    elif text == 'n':
+        return False
+    else:
+        return None       
+
+def setup_players(num_players = 1):
     list_of_players = []
     for i in range(1,num_players+1):
-        player = {'name':f'Player N{i}',
-        'score':0,
-        'is_bot':False,
-        'is winner':False}
+        player = {
+        'name': f'Player N{i}',
+        'score': 0 ,
+        'is_bot': False,
+        'is_winner':False
+        }
         list_of_players.append(player)
     print(list_of_players)
-    return(list_of_players)
+    return list_of_players
 
 def choose_winner(players):
     winners = []
     win_score = []
-    for player in players:
-        if player['is winner'] == True:
-         return player['name']
-        elif player['score'] <21:
+    for player in players: 
+        if player['is_winner'] == True:  #if player['is_winner']
+            return player['name']
+        elif player ['score'] <  21:
             win_score.append(player['score'])
             winners.append(player)
+
     print(winners)
     win_score.sort()
     if winners:
-        for player in winners:
-            if player['score']== win_score[-1]:
+        for player in winners: 
+            if player['score'] == win_score[-1]:
                 return player['name']
     else:
         return 'Нет победителя!'
 
+
+
 s_deck = setup_deck()
+
 while True:
-    num_players = input('Vvedite kolichestvo igrokov')
+    num_players = input('Введите количество игроков: ')
     try:
         num_players = int(num_players)
-        answer = 1/num_players
-        print('succes!')
-    except ZeroDivisionError:
-        print('write a new number, cannot divide by zero!')
     except ValueError:
-        print('enter only numbers!')
+        print('Введите число, не содержащее букв и иных символов! ')
+        continue
+    except Exception as e:
+        print(e)
+        continue
     else:
-        break
+        if num_players>0:
+            break
+        else:
+            print('Введите число больше нуля!')
+            continue        
 
 players = setup_players(num_players)
 
-
 for active_player in players:
-    players_score = active_player['score']
+
+
+    player_score = active_player['score']
     b_continue = True
     while b_continue:
 
         d_card = draw_a_card(s_deck)
-        if d_card != None:
-            print('Вы вытянули: ',d_card)
-            players_score = update_score(players_score,d_card)
-            print(f'У вас {players_score} очков')
-            b_win = count_score(players_score)
+        if d_card != None : # if d_card:
+            print('Вы вытянули:',d_card)
+            player_score = update_score(player_score,d_card)
+            print(f'У вас {player_score} очков')
+            b_win = count_score(player_score)
             if b_win == None:
                 pass
             elif b_win == False:
@@ -103,25 +117,21 @@ for active_player in players:
                 break
             elif b_win == True:
                 print('Вы победили!')
-                active_player['is winner'] = True
-                break
-            
-
+                active_player['is_winner'] = True
+                break    
         else:
-            print('Колода закончилась')
+            print('Колода закончилась!')
             break
         while True:
-            desicion = input('Berem eshe kartu: ')
-            b_continue = draw_decision(desicion)
+            decision = input('Берем еще карту?\ny/n \n')
+            b_continue = draw_decision(decision)
             if b_continue == None:
-                print('Неверная команда')
+                print("Неверная команда")
                 continue
             break
     else:
-        print(f'Вы закончили игру с {players_score} очков')
-    active_player['score'] = players_score
-print(players)
-print(choose_winner(players))
-print('До новых встреч')
-
-
+        print(f'Вы закончили игру с {player_score} очков')
+    active_player['score'] = player_score  
+print(players) 
+print(choose_winner(players))             
+print('До новых встреч!')    
