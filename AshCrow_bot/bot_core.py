8ac5e1378d845f79e3c5d50e71b4aba0e82acbc1
AshCrow137 <56866153+AshCrow137@users.noreply.github.com>
2022-12-22 18:55:43 +0600

    111

--- AshCrow_bot/bot_core.py
+++ AshCrow_bot/bot_core.py
@@ -2,13 +2,19 @@ import requests
 import json
 import time
 
-TOKEN = '5526314084:AAGL5LqSz14D0urgu23ykxoSzWYFkAvaNI8'
+token_file = open('AshCrow_bot/token.txt','rt')
+
+TOKEN = token_file.read()
+
+token_file.close()
 
 METHOD = ''
 
 BOT_URL = f'https://api.telegram.org/bot{TOKEN}/'
 
 OFFSET = 0
+
+
 
 def get_updates(offset):
     METHOD = f'getUpdates?offset={offset}'
@@ -20,10 +26,49 @@ def get_updates(offset):
         return tg_data
     except Exception as er:
         print(er)
+        
+def send_message(chat_id,message_text):
+    user_data = {
+        'chat_id':chat_id,
+        'text':message_text
+    }
+    try:
+        url = BOT_URL+'sendMessage'
+        requests.post(url,data = user_data)
+
+    except Exception as er:
+        print(er)
+
+
+def parse_data(data):
+    if data['result']:
+        offset = data['result'][0]['update_id'] + 1
+        message = data['result'][0]['message']
+        return offset, message
+    else:
+        return None,None
+
+def text_handler(message):
+    text = message['text']
+    chat_id = message['chat']['id']
+
+    if text == '/start':
+        send_message(chat_id, 'Привет!')
+    elif text =='/help':
+         
+        pass
+    elif text =='/hello':
+        pass
+    else:
+        pass
+
 
 while True:
     time.sleep(2)
 
     bot_data = get_updates(OFFSET)    
-    print(bot_data)
+    OFFSET, message = parse_data(bot_data)
+    if message:
+        text_handler(message)
+
 
